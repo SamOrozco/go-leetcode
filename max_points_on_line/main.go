163897a74c823b386f5e4a565cc65e99ec3eaa41
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-01-26 13:36:41 -0800

    starting max points on a line

--- max_points_on_line/main.go
+++ max_points_on_line/main.go
@@ -1,15 +1,37 @@
 package main
 
+import "os"
+
 type Point struct {
 	X int
 	Y int
 }
 
 func main() {
+	points := []Point{
+		{
+			X: 1,
+			Y: 1,
+		},
+		{
+			X: 2,
+			Y: 2,
+		},
+		{
+			X: 3,
+			Y: 3,
+		},
+	}
 
+	if 3 != maxPoints(points) {
+		println("Failed")
+		os.Exit(1)
+	}
 }
 
-
 func maxPoints(points []Point) int {
+	matrix := make([][]int, 0)
+	for _, p := range points {
 
+	}
 }
