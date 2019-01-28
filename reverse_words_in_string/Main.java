6080bf7f0f0d8addac831f7d88d99980d42b05db
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-01-27 16:54:57 -0800

    First attempt and reverse a string

---
+++ reverse_words_in_string/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        if (!"blue is sky the".equals(reverseWords("the sky is blue"))) {
+            System.out.println("Fail");
+            System.exit(1);
+        }
+        System.out.println("pass");
+    }
+
+    public static String reverseWords(String s) {
+        // trim string and split on all white space
+        String[] tokens = s.trim().split("\\s+");
+        int length = tokens.length;
+        StringBuilder bldr = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            if (i != 0) {
+                bldr.append(tokens[i]).append(" ");
+            } else {
+                bldr.append(tokens[i]);
+            }
+        }
+
+        return bldr.toString();
+    }
+}
