b9de09249a9970e0eb31b1d104c3e5b8e4d5674e
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-02-07 21:10:17 -0800

    Adding shortest sub array

---
+++ short_sub_array/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//print(shortestSubarray([]int{1}, 1))
+	print(shortestSubarray([]int{1, 2, 5, 6, 2, 2, 1, 1, 1}, 4))
+}
+
+func shortestSubarray(values []int, sum int) int {
+}
--- wildcard_matching/main.go
+++ wildcard_matching/main.go
@@ -3,18 +3,18 @@ package wildcard_matching
 import "strings"
 
 func main() {
-	println(isMatch("abcdefg", "*a*g"))
+	//println(isMatch("abcdefg", "*a*g"))
 }
 
-func isMatch(s string, p string) bool {
-	if !containsSpecialCharacters(s) { // exact match
-		return s == p
-	}
-	values := []rune(s)
-	for i, v := range p {
-		if v != '*' && v != '?'
-	}
-}
+//func isMatch(s string, p string) bool {
+//	if !containsSpecialCharacters(s) { // exact match
+//		return s == p
+//	}
+//	values := []rune(s)
+//	for i, v := range p {
+//		if v != '*' && v != '?'
+//	}
+//}
 
 func containsSpecialCharacters(val string) bool {
 	return strings.Contains(val, "*") ||
