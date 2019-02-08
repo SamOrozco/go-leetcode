b072cb9b6bc2495c548f5f9db273f8ceef6ce27e
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-02-07 21:14:26 -0800

    reverting

--- short_sub_array/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	//print(shortestSubarray([]int{1}, 1))
-	print(shortestSubarray([]int{1, 2, 5, 6, 2, 2, 1, 1, 1}, 4))
-}
-
-func shortestSubarray(values []int, sum int) int {
-}
