eddd12823b77c64b6cba4af6ec70317f8b7ccd66
SamOrozco <samuel.j.orozco@gmail.com>
2019-05-14 12:01:42 -0700

    pausing on this question for a bit

--- remove_outermost_parenthesis/main.go
+++ remove_outermost_parenthesis/main.go
@@ -1,39 +1,15 @@
-A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+package main
 
-A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+import "os"
 
-Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+func main() {
+	if removeOuterParentheses("(()())(())(()(()))") != "()()()()(())" {
+		println("Failed")
+		os.Exit(1)
+	}
+}
 
-Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
 
- 
-
-Example 1:
-
-Input: "(()())(())"
-Output: "()()()"
-Explanation: 
-The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
-After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
-Example 2:
-
-Input: "(()())(())(()(()))"
-Output: "()()()()(())"
-Explanation: 
-The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
-After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
-Example 3:
-
-Input: "()()"
-Output: ""
-Explanation: 
-The input string is "()()", with primitive decomposition "()" + "()".
-After removing outer parentheses of each part, this is "" + "" = "".
- 
-
-Note:
-
-S.length <= 10000
-S[i] is "(" or ")"
-S is a valid parentheses string
- 
+func removeOuterParentheses(S string) string {
+    return ""
+}
